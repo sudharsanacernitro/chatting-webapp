4a112324f54c74b1cc4754f90ce3604e49e80608
sudharsan <125852798+sudharsanacernitro@users.noreply.github.com>
2023-09-06 14:02:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
-from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivymd.app import MDApp
+from kivy.lang import Builder
 
-
-class LoginScreen(GridLayout):
-
-    def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
-
-
-class MyApp(App):
-
+class SampleApp(MDApp):
+    
     def build(self):
-        return LoginScreen()
+        self.appKv='''
+MDScreen:
+    MDLabel:
+        text:'Hello,World.'
+        multiline:True
+        halign:'center'         
+'''
+        AppScreen=Builder.load_string(self.appKv)
+        return AppScreen
 
-
-if __name__ == '__main__':
-    MyApp().run()
+SampleApp().run()
+    
